ff200c45f4286c0093bc839bd51d7735b8d3cc83
Hameed <hdrhmd@amazon.com>
2020-12-12 01:41:58 -0800

    looks like it is complete now

--- cheatssheet.js
+++ cheatssheet.js
@@ -1,5 +1,6 @@
 const bip39 = require('bip39');
 const crypto = require("crypto");
+const prompt = require('prompt');
 
 const wordToHash = (word, salt) => {
     return crypto.createHmac('sha256', salt)
@@ -22,17 +23,23 @@ const findMinLen = (hashs) => {
     throw 'Hash not unique even at 64 length.';
 }
 
-const words = bip39.wordlists.english;
-const transformed = words.map((w, i) => {
-    return {
-        index: ('000' + i).slice(-4),
-        label: w,
-        octal: indexToOct(i),
-        hash: wordToHash(w, 'password')
-    };
-});
-const minLen = findMinLen(transformed.map(w => w.hash));
-const minified = transformed.map(w => (w.hash = w.hash.slice(0, minLen), w));
-const stringified = minified.map(w => [w.index, w.octal, w.hash, w.label].join(' ')).join('\n');
+prompt.start();
 
-console.log(stringified);
+prompt.get(['salt'], (err, result) => {
+    if (err) throw 'Error getting salt from user.';
+    console.log('Salt: ' + result.salt);
+    const words = bip39.wordlists.english;
+    const transformed = words.map((w, i) => {
+        return {
+            index: ('000' + i).slice(-4),
+            label: w,
+            octal: indexToOct(i),
+            hash: wordToHash(w, result.salt)
+        };
+    });
+    const minLen = findMinLen(transformed.map(w => w.hash));
+    const minified = transformed.map(w => (w.hash = w.hash.slice(0, minLen), w));
+    const stringified = minified.map(w => [w.index, w.octal, w.hash, w.label].join(' ')).join('\n');
+    console.log(stringified);
+});
+
